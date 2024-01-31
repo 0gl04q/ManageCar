f930c42dc0d97185030f5b72cbddc2b7aa7e217e
0gl04q <2000danilrusan2000@gmail.com>
2024-01-31 04:17:23 +0300

    add new file

---
+++ accounts/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.contrib.auth.models import User
+from manual.models import City
+
+
+# class Profile(models.Model):
+#     user = models.OneToOneField(User, on_delete=models.CASCADE)
+#     city = models.ForeignKey(City, on_delete=models.SET_NULL)
+#
+#
+#     def __str__(self):
+#         return self.user.username
